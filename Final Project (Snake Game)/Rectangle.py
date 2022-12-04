c81c43fa46340909666814cb67ac49f69139b29f
Seongrim Choi <113206950+wcsrchoi@users.noreply.github.com>
2022-12-04 02:12:27 -0500

    Add files via upload

---
+++ Final Project (Snake Game)/Rectangle.py
@@ -0,0 +1,34 @@
+"""
+
+This module is a model module for snake head and the body, creating rectangles.
+
+@author Seongrim Choi (sc83)
+@date: Fall, 2021
+"""
+class Rectangle:
+    def __init__(self, x=375, y=275, vel_x=0, vel_y=0, radius=10, color="white"):
+        """Instantiate a particle object."""
+        # Creating a rectangle based on the radius like a cirlce. 
+        self.x = x
+        self.y = y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.radius = radius
+        self.color = color
+    
+    def draw(self, drawing):
+        """ Creating a rectangle object. """
+        drawing.rectangle(self.x - self.radius,
+             self.y - self.radius,
+             self.x + self.radius,
+             self.y + self.radius,
+             color=self.color
+             )
+        
+    def move(self, drawing):
+        """ Moving rectangle object. """
+        self.x += self.vel_x
+        self.y += self.vel_y
+        
+        
+        
